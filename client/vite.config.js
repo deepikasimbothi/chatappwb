c831271f7fb7277ad4c6a47cdd3dc0330bbf4af2
deepikasimbothi <deepikasimbothi@gmail.com>
2024-12-10 12:14:00 +0530

    interface changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5173',
+        target: 'https://chatappweb-he6e.onrender.com',
       }
     }
   },
